1c559f1e0d5b8e269860cc4be4ae0e7ba0a6d910
campb474 <campbellmatt646@gmail.com>
2022-10-29 13:13:38 -0400

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const club = "Web Dev Club";
-  const memberCount = 45;
-  const ecessLink = "https://www.purdue-ecess.org/";
-
   return (
     <div className="App">
+      <Navbar/>
       <div className="Content">
-        <h1>Welcome to { club }</h1>
-        <p>Member Count: { memberCount }</p>
-        <p>{ 1000 }</p>
-        <p>{ "String example" }</p>
-        <p>{ [1, 2, 3, 4, 5, 6, 7] }</p>
-        <p>{ Math.random() * 100 }</p>
-        <a href={ ecessLink }>ECESS Website</a>
+        <Home/>
       </div>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div className="home">
+            <h1>Web Dev Team Home Page</h1>
+        </div>
+    )
+}
+
+export default Home;
